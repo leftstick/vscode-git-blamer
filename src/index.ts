d2c2e935fd546e4bd4c6180b9dba7a74cbb5fa73
Howard.Zuo <leftstick@qq.com>
2017-04-21 15:40:29 +0800

    update a bit

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-'use strict';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
@@ -14,11 +13,12 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    let disposable = vscode.commands.registerCommand('extension.gitblame', () => {
         // The code you place here will be executed every time your command is executed
 
         // Display a message box to the user
         vscode.window.showInformationMessage('Hello World!');
+        console.log('fuck jajaj');
     });
 
     context.subscriptions.push(disposable);
