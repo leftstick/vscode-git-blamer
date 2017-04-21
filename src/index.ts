f78d970cf829a01ad47a277925ef45078f5edbfd
Howard.Zuo <leftstick@qq.com>
2017-04-21 16:54:24 +0800

    add output channel

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as child_process from 'child_process';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -16,9 +17,9 @@ export function activate(context: vscode.ExtensionContext) {
     let disposable = vscode.commands.registerCommand('extension.gitblame', () => {
         // The code you place here will be executed every time your command is executed
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-        console.log('fuck jajaj');
+        var myOutputChannel = vscode.window.createOutputChannel('Git blame');
+        myOutputChannel.show();
+        myOutputChannel.append('Visual Studio Code is awesome!');
     });
 
     context.subscriptions.push(disposable);
